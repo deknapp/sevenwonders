e47888e3fdc90ae26617b0d660246543f311da9d
Nath <nath@alice-knapps-computer-3.local>
2016-11-19 17:11:31 -0900

    basic header files

---
+++ include/Player.h
@@ -0,0 +1,36 @@
+#ifndef Player_H
+#define Player_H
+
+#include <vector>
+
+#include "Card.h"
+#include "Player.h"
+#include "Wonder.h"
+
+
+class Player {
+
+ public:
+	Player();
+	~Player();
+	init(Wonder wonder);
+
+ private:
+
+ 	std::vector<Card> deck;
+ 	std::set<Card> playedCards;
+ 	
+ 	Card disCard;
+ 	Card newCard;
+ 	Player leftNeighbor;
+ 	Player rightNeighbor;
+ 	Wonder wonder;
+ 	int cash;
+
+ 	// prevent generated functions --------------------------------------------
+	Player(const Player&);
+	Player& operator=(const Player&);
+};
+
+#endif /* Player_H */
+
